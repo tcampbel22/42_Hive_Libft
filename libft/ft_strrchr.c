28d0ae9a3b47121630bca7025d8d6d2cb08860a2
Timothy Campbell <tcampbel@c2r4p9.hive.fi>
2023-10-27 14:39:33 +0300

    strchr && strrchr with corrections

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -1,51 +1,49 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tcampbel <tcampbel@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/27 09:48:02 by tcampbel          #+#    #+#             */
-/*   Updated: 2023/10/27 14:16:37 by tcampbel         ###   ########.fr       */
+/*   Created: 2023/10/27 11:47:28 by tcampbel          #+#    #+#             */
+/*   Updated: 2023/10/27 14:38:05 by tcampbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strchr(const char *str, int c)
+char *ft_strrchr(const char *str, int c)
 {
 	char	a;
 
 	a = c;
 	while (*str)
+		str++;
+	while (*str >= 0)
 	{
 		if (*str == a)
 			return ((char*)str);
-	str++;
+	str--;
 	}
-	if (*str == a)
-		return ((char*)str);
 	return (0);
 }
 #include <stdio.h>
 #include <string.h>
 int	main(void)
 {
-	const char	str1[] = "aaaaaaabaa";
-	const char	str2[] = "fff";
-	int	c = 'b';
+	const char	str1[] = "baaa0b0aaa";
+	const char	str2[] = "";
+	int	c = 'c';
 	int	d = '\0';
 	char* ret;
 	char* ret1;
 	char* ret2;
 	char* ret3;
 
-	ret = ft_strchr(str1, c);
-	ret1 = strchr(str1, c);
-	ret2 = ft_strchr(str2, d);
-	ret3 = strchr(str2, d);
-	printf("OG Function says = %s\n", ret1);
-	printf("FT Funtion says = %s\n", ret);
-	printf("OG Funtion says = %s\n", ret3);
-	printf("FT Funtion says = %s\n", ret2);
+	ret = ft_strrchr(str1, c);
+	ret1 = strrchr(str1, c);
+	ret2 = ft_strrchr(str2, d);
+	ret3 = strrchr(str2, d);
+	printf("FT Function says = %s\n", ret);
+	printf("OG Funtion says = %s\n", ret1);
+	printf("FT Funtion says = %s\n", ret3);
+	printf("OG Funtion says = %s\n", ret2);
 }
-
-
