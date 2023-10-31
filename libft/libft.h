798983932478c2b4a3be7146abba0e193d2afc6e
Timothy Campbell <tcampbel@c2r4p4.hive.fi>
2023-10-31 19:24:47 +0200

    norm error updates

--- libft/libft.h
+++ libft/libft.h
@@ -6,23 +6,22 @@
 /*   By: tcampbel <tcampbel@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 17:54:19 by tcampbel          #+#    #+#             */
-/*   Updated: 2023/10/31 19:18:32 by tcampbel         ###   ########.fr       */
+/*   Updated: 2023/10/31 19:24:20 by tcampbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#ifndef	LIBFT_H
-#define	LIBFT_H
+#ifndef LIBFT_H
+#define LIBFT_H
 
 #include <stdlib.h>
-#include <unistd>
+#include <unistd.h>
 
-int	ft_isalnum(int c);
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-int	ft_tolower(int c);
-int	ft_toupper(int c)
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
 size_t	ft_strlen(const char *s);
 void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
@@ -32,11 +31,11 @@ size_t	ft_strlcat(char *dest, const char *src, size_t dstsize);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 char	*ft_strchr(const char *str, int c);
 char	*ft_strrchr(const char *str, int c);
-int	ft_strncmp(const char *str1, const char *str2, size_t n);
+int		ft_strncmp(const char *str1, const char *str2, size_t n);
 void	*ft_memchr(const void *str, int c, size_t n);
-int	ft_memcmp(const void *str1, const void *str2, size_t n);
+int		ft_memcmp(const void *str1, const void *str2, size_t n);
 char	*ft_strnstr(const char *hay, const char *needle, size_t n);
-int	ft_atoi(const char *str);
+int		ft_atoi(const char *str);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(char *src);
 
